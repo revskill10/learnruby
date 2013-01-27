66c1aff71ac3034dbfe01dae7f6bb77d7dc5467a
checkraiser <checkraiser11@gmail.com>
2013-01-27 18:52:54 +0700

    Update service.rb
    Now come the service.
    1) Fetch all classes, subjects and students lists.
    2) Fetch all timetable of classes
    3) Update slots for each class and student.

--- service.rb
+++ service.rb
@@ -3,7 +3,7 @@ require 'mongoid'
 require 'savon'
 require 'date'
 require 'time'
-Mongoid.load!("mongoid_service.yml", :production);
+Mongoid.load!("config/mongoid.yml", :production);
 
   class Monhoc
     include Mongoid::Document
@@ -41,13 +41,61 @@ Mongoid.load!("mongoid_service.yml", :production);
     has_and_belongs_to_many :sinhviens
     belongs_to :lopmonhoc    
   end
-  @@tiets = ['6:30','7:20','8:10','9:05','9:55','10:45','12:50','13:40','14:30','15:25','16:15','17:05'];
+  @@tiets = ['06:30','07:20','08:10','09:05','09:55','10:45','12:50','13:40','14:30','15:25','16:15','17:05'];
   @@htiets = @@tiets.map {|h| DateTime.strptime(h,"%H:%M")}  
   @@thu = {:thu2 => 0, :thu3 => 1,:thu4 => 2,:thu5 => 3,:thu6 => 4,:thu7 => 5,:thu8 => 6};  
 before do      
   content_type 'application/json'
 end
 
+get '/capnhattkb' do 
+  client = Savon::Client.new do |wsdl, http|
+    wsdl.document = "http://10.1.0.238:8082/HPUWebService.asmx?wsdl"
+    http.read_timeout = 7*24*3600
+  end  
+  
+
+  response = client.request(:tkb_lop_mon_hoc) ;    
+  res_hash = response.body.to_hash if response and response.body;
+  ls = res_hash[:tkb_lop_mon_hoc_response][:tkb_lop_mon_hoc_result][:diffgram][:document_element];   
+  cc = 0
+  if (ls) then         
+    ls[:tkb_lop_mon_hoc].each do |item|                
+      lop = Lopmonhoc.find_by(malop: item[:ma_lop].strip) rescue nil 
+      puts item.inspect ; next if lop == nil      
+      cc = cc + 1
+      puts "process " + cc.to_s + " :" + item[:ma_lop].strip
+      if (lop.sotinchi == nil) then 
+        lop.update_attributes(
+              tengiaovien: item[:ten_giao_vien] ? item[:ten_giao_vien].strip : '',
+              maphonghoc: item[:ma_phong_hoc].is_a?(String) ? item[:ma_phong_hoc].strip : '',
+              sotinchi: item[:sotc].strip,
+              namhoc: item[:nam_hoc].strip,
+              hocky: item[:hoc_ky].strip)   
+      end   
+      ngaybatdau = DateTime.strptime(item[:tu_ngay].strip,"%d/%m/%Y")                                 
+      @@thu.each do |k,v|
+        if item.has_key?(k) then
+          index = item[k].strip.to_i-1;
+          ngaybatdau = DateTime.new(ngaybatdau.year, ngaybatdau.month, ngaybatdau.day,
+            @@htiets[index].hour, @@htiets[index].minute) + v;
+          ngayketthuc = DateTime.strptime(item[:ngay_ket_thuc].strip,"%d/%m/%Y");
+          sl = Slot.find_or_create_by(batdau: Time.zone.parse(ngaybatdau.to_s), lopmonhoc_id: lop.id)                                  
+          lop.slots << sl               
+          dt = ngaybatdau
+          while (dt + 7) < ngayketthuc do
+            dt = DateTime.new(dt.year, dt.month, dt.day , dt.hour, dt.minute) + 7;
+            sl = Slot.find_or_create_by(batdau: Time.zone.parse(dt.to_s), lopmonhoc_id: lop.id)                
+            lop.slots << sl
+          end
+        end
+      end
+    end  
+  end
+    
+  
+  return {:result => "OK"}.to_json  
+end
 
 get '/getall' do  
   #Resque.enqueue(Archive, ip, Time.now.to_s, msv, 'checkltn')
@@ -57,96 +105,55 @@ get '/getall' do
   ls = res_hash[:sinh_vien_lop_mon_hoc_response][:sinh_vien_lop_mon_hoc_result][:diffgram][:document_element]; 
   ls[:sinh_vien_lop_mon_hoc].each do |item|
     m = Monhoc.find_or_create_by(mamonhoc: item[:ma_mon_hoc].strip)    
+    m.update_attributes(tenmonhoc: item[:ten_mon_hoc].strip)
     l = Lopmonhoc.find_or_create_by(malop: item[:ma_lop].strip)        
-    m.lopmonhocs << l
+    m.lopmonhocs << l if l != nil
     s = Sinhvien.find_or_create_by(masinhvien: item[:ma_sinh_vien].strip)    
-    l.sinhviens << s
+    l.sinhviens << s if s != nil
   end  
   return {:result => ls[:sinh_vien_lop_mon_hoc][0]}.to_json  
 end
 
-get '/capnhattkb' do 
-  client = Savon::Client.new do |wsdl, http|
-    wsdl.document = "http://10.1.0.238:8082/HPUWebService.asmx?wsdl"
-    http.read_timeout = 24*3600
-  end
-  
-  Sinhvien.each do |sv| 
-    msv = sv.masinhvien        
-    response = client.request(:tkb) do
-      soap.body = {:masinhvien => msv}
-    end  
-    res_hash = response.body.to_hash if response and response.body;
-    ls = res_hash[:tkb_response][:tkb_result][:diffgram][:document_element]; 
-    if (ls ) then 
-      temp = ls[:tkb];
-      if (temp.is_a?(Hash)) then 
-        ls = Array.new
-        ls.push(temp);
-      else (temp.is_a?(Array))
-        ls = temp;
-      end
-    else 
-      puts "1 mon: " + msv;
-      #return '{"error":"error1"} '
-    end
-    puts "processing " + msv;
-    if (ls) then 
-      ls.each do |item|        
-        ngaybatdau = DateTime.strptime(item[:tu_ngay].strip,"%d/%m/%Y")
-        lop = Lopmonhoc.find_by(malop: item[:ma_lop].strip)        
-        @@thu.each do |k,v|
-          if item.has_key?(k) then
-            index = item[k].strip.to_i-1;
-            ngaybatdau = DateTime.new(ngaybatdau.year, ngaybatdau.month, ngaybatdau.day,
-              @@htiets[index].hour, @@htiets[index].minute) + v;
-            ngayketthuc1 = DateTime.strptime(item[:ngay_ket_thuc].strip,"%d/%m/%Y");
-            sl = Slot.find_or_create_by(batdau: Time.parse(ngaybatdau.to_s), lopmonhoc_id: lop.id)                                  
-            lop.slots << sl   
-            lop.update_attributes(tungay: Time.parse(DateTime.strptime(item[:tu_ngay].strip,"%d/%m/%Y").to_s),
-                tengiaovien: item[:ten_giao_vien] ? item[:ten_giao_vien].strip : '',
-                maphonghoc: item[:ma_phong_hoc].instance_of?(String) ? item[:ma_phong_hoc].strip : '',
-                sotinchi: item[:sotc].strip,
-                namhoc: item[:nam_hoc].strip,
-                hocky: item[:hoc_ky].strip,
-                sotuanhoc: item[:so_tuan_hoc].strip,
-                ngayketthuc: Time.parse(ngayketthuc1.to_s),
-                siso: lop.sinhviens.size)                 
-            dt = ngaybatdau
-            while (dt + 7) <= ngayketthuc1 do
-              dt = DateTime.new(dt.year, dt.month, dt.day , dt.hour, dt.minute) + 7;
-              sl = Slot.find_or_create_by(batdau: Time.parse(dt.to_s), lopmonhoc_id: lop.id)                
-              lop.slots << sl
-            end
-          end
+get '/sinhvien/truc/?' do   
+  Lopmonhoc.each do |lop|      
+    puts "process " + lop.malop 
+    sinhviens = lop.sinhviens.shuffle
+    slots = lop.slots if lop 
+    count = sinhviens.size if sinhviens
+    temp = 0
+    if (count > 0 and slots.size > 0) then
+      slots.each do |slot|   
+        tt = (count >= 3) ? 3 : count;   
+        tt.times do         
+          if (temp >= count) then  temp = 0; end
+          slot.sinhviens << Sinhvien.find_by(masinhvien: sinhviens[temp].masinhvien)
+          temp = temp + 1
         end
-      end  
-    end
-  end
-  return {:result => ls}.to_json  
-end
- 
-
-get '/sinhvien/:malop' do |malop|
-  lop = Lopmonhoc.find_by(malop: malop.strip)
-  sinhviens = lop.sinhviens
-  slots = lop.slots
-  count = sinhviens.size
-  temp = 0
-  if (count > 0 and slots.size > 0) then
-    slots.each do |slot|      
-      3.times do         
-        if (temp >= count) then  temp = 0; end
-        slot.sinhviens << sinhviens[temp]
-        temp = temp + 1
       end
     end
   end
-  return lop.slots.map {|sl| sl.sinhviens.map {|sv| sv.masinhvien} }.to_json
+  return {:res => "OK"}.to_json
 end
-get '/slots/:malop' do |malop|
-  lop = Lopmonhoc.find_by(malop: malop.strip)
-  return lop.slots.map {|sl| sl.batdau }.to_json
+get '/lops/?' do 
+  return Lopmonhoc.all.map {|lop| lop.malop}.to_json 
 end
+get '/sinhviens/?' do
+  return Sinhvien.all.map {|sv| sv.masinhvien}.to_json 
+end
+get '/lop/:malop' do |malop|
+  ml = malop.strip
 
+  lop = Lopmonhoc.find_by(malop: ml)
+
+  return lop.slots.map {|sl| {:batdau => sl.batdau.new_offset(Rational(utc_offset, 24)), :sv => sl.sinhviens.map {
+      |sv| sv.masinhvien 
+    } } }.to_json
+end
+get '/sinhvien/:sv' do |sv|
+  msv = sv.strip
+  utc_offset = 0
+  ssv = Sinhvien.find_by(masinhvien: msv)
+  puts ssv.slots[0].batdau.to_s 
+  return ssv.slots.map {|sl| {:batdau => sl.batdau.new_offset(Rational(utc_offset, 24)), :lop => sl.lopmonhoc.malop } }.to_json
+end
 
